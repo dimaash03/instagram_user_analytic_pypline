7fecf800a7891ba6dd9bac0fb7af15f3b2a81335
dimaash03 <dima.shpak.2015ukraine@gmail.com>
2026-02-25 12:46:29 +0100

    Add SQL query for Question 2 average engagement by country

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,8 +1,25 @@
-SELECT 
+-- Q1: Which countries have the largest user base?
+SELECT
 	country,
 	COUNT(*) AS user_count,
 	ROUND(100.00 * COUNT(*) / (SELECT COUNT(*) FROM data), 2) AS user_pct
 FROM data
 GROUP BY country
 ORDER BY user_count DESC;
-/*SQL query to get country-wise user counts and percentages */
+
+-- Q2: Average engagement metrics by country
+SELECT
+    country,
+    COUNT(*) AS users,
+    ROUND(AVG(daily_active_minutes_instagram), 2) AS avg_minutes_per_user,
+    ROUND(AVG(stories_viewed_per_day), 2) AS avg_stories_per_user,
+    ROUND(AVG(reels_watched_per_day), 2) AS avg_reels_per_user,
+    ROUND(AVG(sessions_per_day), 2) AS avg_sessions_per_user,
+    ROUND(AVG(likes_given_per_day), 2) AS avg_likes_per_user,
+    ROUND(AVG(comments_written_per_day), 2) AS avg_comments_per_user,
+    ROUND(AVG(dms_sent_per_week), 2) AS avg_dms_per_user
+FROM data
+GROUP BY country
+ORDER BY avg_minutes_per_user DESC;
+
+
